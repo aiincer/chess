7b20358ec6c42672699dad88799d820354b20e60
no-name-coding <natur2.stadion@icloud.com>
2026-07-20 18:47:17 +0200

    (ssh) (#profile-page / isu:16) bild aus session-storage abfragen

--- docs/src/assets/profile.js
+++ docs/src/assets/profile.js
@@ -1,8 +1,15 @@
 function loadProfile(id) {
+    const profilePicData = JSON.parse(sessionStorage.getItem("profile-picture"));
+
     const div = document.getElementById(id);
     if (!div) return;
 
-    div.style.backgroundImage = "url('https://aiincer.github.io/favicon.ico')";
+    const images = {
+        "txt-prof": "https://aiincer.github.io/favicon.ico"
+    };
+    const image = images[profilePicData.picture];
+
+    div.style.backgroundImage = `url(${image})`;
     div.style.backgroundSize = "cover";
     div.style.backgroundPosition = "center";
     div.style.backgroundRepeat = "no-repeat";
