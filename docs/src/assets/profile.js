d523519bc9a71bf8bab16385bf27b9102d75085f
no-name-coding <natur2.stadion@icloud.com>
2026-07-20 18:54:46 +0200

    (ssh) (#profile-page / isu:16) lokales bild-hosting

--- docs/src/assets/profile.js
+++ docs/src/assets/profile.js
@@ -1,15 +1,15 @@
-function loadProfile(id) {
+function loadProfile(id, pre) {
+    //session-storage
     const profilePicData = JSON.parse(sessionStorage.getItem("profile-picture"));
-
+    //select
     const div = document.getElementById(id);
     if (!div) return;
-
+    //Bild
     const images = {
-        "txt-prof": "https://aiincer.github.io/favicon.ico"
+        "txt-prof": "standard.png"
     };
-    const image = images[profilePicData.picture];
-
-    div.style.backgroundImage = `url(${image})`;
+    const image = "src/img/profilePics/" + images[profilePicData.picture];
+    div.style.backgroundImage = `url(${pre}${image})`;
     div.style.backgroundSize = "cover";
     div.style.backgroundPosition = "center";
     div.style.backgroundRepeat = "no-repeat";
